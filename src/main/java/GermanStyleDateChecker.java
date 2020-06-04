ece8df824a6f570b056424c33aaa1745f001126a
Marc Kruiß <k.marc@a1.net>
2020-06-04 10:06:32 +0200

    Result

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,6 +3,8 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        return null;
+        String regex = "\\A((3([01]))|([0-2]?[1-9]))(\\.|-| )((1[0-2])|(0?[1-9]))(\\.|-| )((19\\d\\d)|((20)?\\d\\d))\\Z";
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(dateString);
     }
 }
